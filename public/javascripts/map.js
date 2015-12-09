b60ba22c63f2803b4620b78a4476ad3f28d96774
markitos andré <markitos andr>
2015-12-09 22:03:32 +0000

    GDI Implement first features on the map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,9 +1,38 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([40.646782579355886, -8.687095642089844], 11);
+map.setMaxBounds([[40.760260692426165, -8.276824951171875], [40.499703081749566, -8.955917358398438]]);
 
+var nominating = new L.Control.Geocoder.Nominatim({
+    serviceUrl:'http://nominatim.openstreetmap.org/',
+    geocodingQueryParams: {
+        countrycodes:'pt'
+    }});
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+var nexrad = L.tileLayer.wms("http://localhost:8080/geoserver/GDI/wms", {
+    layers: 'GDI:teste',
+    format: 'image/png',
+    transparent: true,
+    attribution: ""
+}).addTo(map);
+
+var geocoder = new L.Control.Geocoder({geocoder: nominating}).addTo(map);
+
+var marker;
+map.on('click', function(e) {
+    nominating.reverse(e.latlng, map.options.crs.scale(map.getZoom()), function(results) {
+        var r = results[0];
+        if (r) {
+            if (marker) {
+                marker.
+                setLatLng(r.center).
+                setPopupContent(r.html || r.name).
+                openPopup();
+            } else {
+                marker = L.marker(r.center).bindPopup(r.name).addTo(map).openPopup();
+            }
+        }
+    })
+})
+
