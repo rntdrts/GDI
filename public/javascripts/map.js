904f0e15960d1059402becc68def6d829110ace5
renato duarte <rntdrts@github.com>
2015-12-12 16:41:30 +0000

    GDI Setting style and info Control to map

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -40,41 +40,146 @@ var wfs = L.WFS('http://localhost:8080/geoserver/GDI/ows', {namespace: 'GDI'});
 
 var secundarySchools, supermarkets, restaurants, convenienceStores, universities, pharmacies, houses, apartments;
 wfs.getFeature(['secundary_schools'], {}, function(result, error) {
-    secundarySchools = new L.geoJson(JSON.parse(result));
+    secundarySchools = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#F5C66A'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(secundarySchools);
 });
 
 wfs.getFeature(['supermarket'], {}, function(result, error) {
-    supermarkets = new L.geoJson(JSON.parse(result));
+    supermarkets = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#6AF5D1'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(supermarkets);
 });
 wfs.getFeature(['restaurant'], {}, function(result, error) {
-    restaurants = new L.geoJson(JSON.parse(result));
+    restaurants = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#F5866A'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(restaurants);
 });
 
 
 wfs.getFeature(['loja_conveniencia'], {}, function(result, error) {
-    convenienceStores = new L.geoJson(JSON.parse(result));
+    convenienceStores = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#EAF56A'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(convenienceStores);
 });
 
 wfs.getFeature(['university'], {}, function(result, error) {
-    universities = new L.geoJson(JSON.parse(result));
+    universities = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#249D35'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(universities);
 });
 
 wfs.getFeature(['farmacy'], {}, function(result, error) {
-    pharmacies = new L.geoJson(JSON.parse(result));
+    pharmacies = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#A86AF5'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(pharmacies);
 });
 
 wfs.getFeature(['house'], {}, function(result, error) {
-    houses = new L.geoJson(JSON.parse(result));
+    houses = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#1C5019'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(houses);
 });
 
 wfs.getFeature(['apartments'], {}, function(result, error) {
-    apartments = new L.geoJson(JSON.parse(result));
+    apartments = new L.geoJson(JSON.parse(result), {
+        style: function(feature) {
+            return {color: '#AFCAFF'};
+        },
+        pointToLayer: function(feature, latlng) {
+            return new L.CircleMarker(latlng, {radius: 5, fillOpacity: 0.85});
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.name);
+        }
+    });
     map.addLayer(apartments);
 });
+
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        data = [
+            {color: '#F5C66A', name: 'Escolas Secundárias'},
+            {color: '#6AF5D1', name: 'Super-Mercados'},
+            {color: '#F5866A', name: 'Restaurantes'},
+            {color: '#EAF56A', name: 'Lojas de Conveniência'},
+            {color: '#249D35', name: 'Universidades'},
+            {color: '#A86AF5', name: 'Farmácias'},
+        ];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < data.length; i++) {
+        div.innerHTML += '<i style="background:' + data[i].color + '"></i> &ndash;' + data[i].name + '<br>';
+    }
+
+    return div;
+};
+
+legend.addTo(map);
